b5266ea60f6f5f6e163d06864ceb21d033cd0e38
jvalladares <chevalladares@gmail.com>
2023-07-18 13:31:46 -0400

    fixes test error

--- app/src/main/java/com/example/desafio/domain/SyncHitsUseCase.kt
+++ app/src/main/java/com/example/desafio/domain/SyncHitsUseCase.kt
@@ -2,7 +2,6 @@ package com.example.desafio.domain
 
 import com.example.desafio.domain.commons.UseCase
 import com.example.desafio.domain.repository.HitRepository
-import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class SyncHitsUseCase @Inject constructor(
@@ -10,8 +9,6 @@ class SyncHitsUseCase @Inject constructor(
 ) : UseCase<List<HitDomain>, Unit> {
 
     override suspend fun run(params: Unit): Result<List<HitDomain>> {
-        delay(2000L)
-        throw Exception("ass")
         return repository.getHits()
     }
 }
